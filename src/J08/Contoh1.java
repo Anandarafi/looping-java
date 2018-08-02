80dc4530437e7972c04e1c1223edb322ca6050a0
Anandarafi <Anandarafi@DESKTOP-OQL60SH.mshome.net>
2018-08-02 07:47:13 +0700

    For While Do-While

---
+++ src/J08/Contoh1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J08;
+
+/**
+ *
+ * @author Anandarafi
+ */
+public class Contoh1 {
+    public static void main(String[] args)
+    {
+        //PERULANGAN FOR
+        int x;
+        for(x = 1 ; x <= 5 ; x++);
+        {
+            System.out.println("SMK TELKOM MALANG");
+        }
+        
+        //PERULANGAN WHILE
+        int i = 1;
+        while(i <= 5)
+        {
+            System.out.println("SMK TELKOM MALANG");
+            i++;
+        }       
+        int j = 1;
+        do
+        {
+            System.out.println("SMK TELKOM MALANG");
+            j++;
+        }
+        while(j <= 5);
+    }
+}
